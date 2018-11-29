786ea583d7cef41237daeb4e1e3befa6e7867a58
Watinguyaz <watimakhanu@gmail.com>
2018-11-30 02:09:32 +0300

    it will add if statements to determine triangle type

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,27 @@ function triangle() {
   sides.push(document.getElementById("side3").value);
 
   var [a,b,c] = sides;
-  
+
   var display = document.getElementById("display");
     if(a.length === 0 || b.length === 0 || c.length === 0 ) {
       alert("Please fill in all the fields")
       confirm("Have you understood all instructions? click on help")
     } else {
       side(a,b,c);
+    }
+  function side(a,b,c) {
+      if(a + b > c && b + c > a && c + a > b) {
+        if(a === b && a === c) {
+          display.innerHTML = "<p>This is an Equilateral triangle</p>"
+        }
+        else if(a === b || b === c || a === c) {
+          display.innerHTML = "This is an Isoceles triangle"
+        }
+        else {
+          display.innerHTML = "This is a Scalene triangle"
+        }
+      }
+      else {
+        display.innerHTML = "This is not a triangle"
+      }
     }
