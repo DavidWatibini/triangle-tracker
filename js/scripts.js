171e6bc35cceb93947dbf24f6d73a44f103ae8e9
Watinguyaz <watimakhanu@gmail.com>
2018-11-30 02:03:41 +0300

    it will add a function and an  array to scripts.js file

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function triangle() {
+  var sides = [];
+  sides.push(document.getElementById("side1").value);
+  sides.push(document.getElementById("side2").value);
+  sides.push(document.getElementById("side3").value);
+
+  var [a,b,c] = sides;
