3d4a7f2281e1aebec57e4b930e931def27fa1e99
Watinguyaz <watimakhanu@gmail.com>
2018-11-30 02:06:31 +0300

    it will add first if statement to determine input

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,11 @@ function triangle() {
   sides.push(document.getElementById("side3").value);
 
   var [a,b,c] = sides;
+  
+  var display = document.getElementById("display");
+    if(a.length === 0 || b.length === 0 || c.length === 0 ) {
+      alert("Please fill in all the fields")
+      confirm("Have you understood all instructions? click on help")
+    } else {
+      side(a,b,c);
+    }
